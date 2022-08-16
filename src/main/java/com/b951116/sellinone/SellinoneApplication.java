8d227ffbb7cd3223f623bf7610b6e117c723d71f
HeoYou <lov21y_@naver.com>
2022-08-16 23:05:18 +0900

    First Commit

---
+++ src/main/java/com/b951116/sellinone/SellinoneApplication.java
@@ -0,0 +1,13 @@
+package com.b951116.sellinone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellinoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellinoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b951116/sellinone/SellinoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b951116.sellinone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellinoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
